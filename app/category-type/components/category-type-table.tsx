053359d8244d04d815640a1ccb27942e2c63b5c6
Alain Ruales <alainmateo01@gmail.com>
2025-01-21 20:40:30 -0500

    feat: implement CategoryTypeTable component and integrate it into the home page

--- app/category-type/components/category-type-table.tsx
+++ app/category-type/components/category-type-table.tsx
@@ -1,23 +1,19 @@
-'use server';
+
 import { categoryTypeRepository } from '../../shared/services';
 export default async function CategoryTypeTable()
 {
     const categoryTypes = await categoryTypeRepository.getAll();
-    let count = 1;
     return (
         <table>
             <thead>
                 <tr>
-                    <th>#</th>
                     <th>Nombre</th>
                 </tr>
             </thead>
             <tbody>
                 {categoryTypes.map(categoryType => (
                     <tr key={categoryType.id}>
-                        <td>{count}</td>
                         <td>{categoryType.name}</td>
-                        {count += 1}
                     </tr>
                 ))}
             </tbody>
---
+++ app/category-type/page.tsx
@@ -0,0 +1,12 @@
+import CategoryTypeTable from "./components/category-type-table";
+
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <CategoryTypeTable />
+      </main>
+    </div>
+  );
+}
